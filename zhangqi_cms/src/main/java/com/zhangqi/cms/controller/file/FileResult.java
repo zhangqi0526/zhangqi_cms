0cb72b321a1ce095b721591ceca51445bef70f0f
zhangqidage <1807109727@qq.com>
2019-12-13 14:27:02 +0800

    文章管理 图片上传

---
+++ zhangqi_cms/src/main/java/com/zhangqi/cms/controller/file/FileResult.java
@@ -0,0 +1,32 @@
+package com.zhangqi.cms.controller.file;
+
+public class FileResult {
+	public FileResult(int error, String url) {
+		super();
+		this.error = error;
+		this.url = url;
+	}
+
+	int error = 0;
+	String url = "";
+
+	public FileResult() {
+		super();
+	}
+
+	public int getError() {
+		return error;
+	}
+
+	public void setError(int error) {
+		this.error = error;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
